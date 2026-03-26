89ad868a4bf2b4c3b16239a2300a0136d3195435
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-26 17:23:37 +0500

    Refactor Prisma configuration and schema: update DATABASE_URL access method and make description field optional in Task model for improved flexibility.

---
+++ client/prisma/migrations/20260326122252_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `priority` VARCHAR(191) NOT NULL DEFAULT 'medium',
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `dueDate` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
