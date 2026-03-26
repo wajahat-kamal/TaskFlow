09c03c82f284e571b6be556d2c3a3d47a4fc516d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-26 10:48:20 +0500

    Update tsconfig to include DashboardStats file and refactor task statistics calculation for improved accuracy.

--- client/lib/DashboardStats.ts
+++ client/lib/DashboardStats.ts
@@ -1,16 +1,16 @@
 import { tasks } from "@/data/tasksDumydata"
 import { StatsData } from "@/types/DashboardTypes"
+import { isOverDue } from "./TaskCardFunctions";
 
-const totalTasks = tasks.length;
 const pendingTasks = tasks.filter((task) => !task.completed).length
 const completedTasks = tasks.filter((task) => task.completed).length
-// const overdue = tasks
+const overdue = tasks.filter((task) => !task.completed && isOverDue(task.dueDate)).length
 
 const statsData: StatsData[] = [
-    { title: "Total", numbers: totalTasks, para: "all tasks", numColor: "text-white" },
+    { title: "Total", numbers: tasks.length, para: "all tasks", numColor: "text-white" },
     { title: "Pending", numbers: pendingTasks, para: "not completed", numColor: "text-amber-400" },
     { title: "Completed", numbers: completedTasks, para: "completed", numColor: "text-green-400" },
-    { title: "Overdue", numbers: 4, para: "past due date", numColor: "text-rose-400" },
+    { title: "Overdue", numbers: overdue, para: "past due date", numColor: "text-rose-400" },
 ]
 
 export default statsData
