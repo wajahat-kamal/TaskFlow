1295be396ddfb5c9d80259785702702a4c4c2916
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-27 10:58:51 +0500

    Remove unused GET route for fetching tasks and delete related file

--- client/app/api/tasks/get-tasks/route.ts
+++ client/app/api/tasks/get-tasks/route.ts
@@ -2,6 +2,6 @@ import pool from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const [rows] = await pool.query("SELECCT * FROM tasks")
+    const [rows] = await pool.query("SELECT * FROM tasks")
     return NextResponse.json(rows)
 }
