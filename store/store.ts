7c54683b14bc62fc02c7a98c6b14b69a682328fe
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-28 12:53:49 +0500

    Update package dependencies: add zustand for state management and change dev to devOptional for improved dependency handling in package-lock.json.

---
+++ store/store.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+
+interface StoreState {
+    createTaskPopup: boolean
+    toggleCreateTaskPopup: () => void
+}
+
+const useStore = create<StoreState>((set, get) => ({
+    createTaskPopup: false,
+    toggleCreateTaskPopup: () => set({ createTaskPopup: !get().createTaskPopup }),
+}))
+
+export const isCreateTaskPopupOpen = useStore((state) => state.createTaskPopup)
+export const toggleCreateTaskPopup = useStore((state) => state.toggleCreateTaskPopup)
