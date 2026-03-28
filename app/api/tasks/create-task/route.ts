d2b38cf8d6072f446e1c7ba9f60d20565f4ae104
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-28 07:38:32 +0500

    Enhance task creation route: integrate user authentication to ensure only authorized users can create tasks, and improve error handling with more informative responses.

--- app/api/tasks/create-task/route.ts
+++ app/api/tasks/create-task/route.ts
@@ -1,10 +1,14 @@
 import pool from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
-
 
 export async function POST(req: NextRequest) {
     try {
-        const { userId, title, description, priority, dueDate } = await req.json()
+        const { title, description, priority, dueDate } = await req.json()
+        const { userId } = await auth()
+        if (!userId) {
+            return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 })
+        }
         if (!title) {
             return NextResponse.json({ success: false, message: "Title must required!" })
         }
@@ -14,9 +18,9 @@ export async function POST(req: NextRequest) {
             [userId, title, description ?? null, priority, dueDate]
         )
 
-        return NextResponse.json({ success: true, message: 'Task created' })
+        return NextResponse.json({ success: true, message: 'Task created', task })
     } catch (error) {
-        console.log(error);
-
+        console.log(error)
+        return NextResponse.json({ success: false, message: "Something went wrong" }, { status: 500 })
     }
 }
