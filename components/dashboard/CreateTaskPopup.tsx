6491210fd3a23a23c798ba7f6ab7b31b87da1e07
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-28 13:19:08 +0500

    Implement task creation form in CreateTaskPopup: add input fields for title, description, priority, and due date, and integrate API call for task submission.

--- components/dashboard/CreateTaskPopup.tsx
+++ components/dashboard/CreateTaskPopup.tsx
@@ -1,14 +1,38 @@
 "use client"
 import useStore from '@/store/store';
+import axios from 'axios';
 import { X } from 'lucide-react';
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreateTaskPopup() {
     const isCreateTaskPopupOpen = useStore((state) => state.createTaskPopup)
     const toggleCreateTaskPopup = useStore((state) => state.toggleCreateTaskPopup)
 
+    const [taskData, setTaskData] = useState({
+        title: "",
+        description: "",
+        priority: "",
+        dueDate: "",
+    })
 
     if (!isCreateTaskPopupOpen) return;
+
+    const onChange = (e) => {
+        e.preventDefault();
+
+    }
+
+    const createTask = async () => {
+        try {
+            const { data } = await axios.post('http://localhost:3000/api/tasks/create-task', taskData)
+            if (data.success) {
+                console.log('Task created!')
+            }
+        } catch (error) {
+            console.log("task creating error");
+        }
+
+    }
     return (
         <div className='w-screen min-h-screen flex justify-center items-center bg-black/10 backdrop-blur-sm absolute top-0 right-0 z-30'>
             <div className='w-70 min-h-80 p-4 flex justify-center items-center flex-col bg-foreground'>
@@ -17,7 +41,26 @@ function CreateTaskPopup() {
                     <button onClick={toggleCreateTaskPopup}><X /></button>
                 </div>
                 <form>
-                    div
+                    <div>
+                        <label htmlFor="title">Title</label>
+                        <input onChange={onChange} type="text" placeholder='Enter Title' name='title' />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <input onChange={onChange} type="text" placeholder='Enter Description' name='description' />
+                    </div>
+                    <div>
+                        <label htmlFor="priority">Priority</label>
+                        <select name="priority" id="">
+                            <option value="high">High</option>
+                            <option value="medium">Medium</option>
+                            <option value="low">Low</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="date">Due Date</label>
+                        <input onChange={onChange} type="date" placeholder='Enter Due Date' name='date' />
+                    </div>
                 </form>
             </div>
         </div>
